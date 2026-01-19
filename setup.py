33b45e6ced66d45594edc892c1e5c760b88bd971
medinammartin3 <medinammartin3@gmail.com>
2026-01-18 23:13:00 -0500

    Add Python setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ift6758',
+    packages=find_packages(),
+    # package_dir={'': 'src'},
+    version='0.2.0',
+    description='Sample project repo for IFT6758-2021',
+    author='Rémi Lalonde, Martin Medina, Guillaume Collin',
+    license='',
+)
